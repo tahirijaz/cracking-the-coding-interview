8ffce349ab691dc9e6d3b8432ac1851d4b4a3611
Tahir Ijaz <tahir@macbookair.1201indiana.com>
2016-12-12 19:58:19 -0500

    First Problem :)

---
+++ chapter1/1.1/IsUnique.java
@@ -0,0 +1,20 @@
+// Problem 1.1 - Check String has all unique characters no extra space
+
+public class IsUnique{
+  private boolean isUnique(String str){
+    long bitVector = 0;
+    for(int i=0; i<str.length(); i++){
+      int ch = (int)(str.charAt(i) - 'a');
+      if(((1<<ch) & bitVector) > 0){
+        return false;
+      }
+      bitVector |= (1<<ch);
+    }
+    return true;
+  }
+
+  public static void main(String[] args){
+    IsUnique unique = new IsUnique();
+    System.out.println(unique.isUnique("tahir"));
+  }
+}
