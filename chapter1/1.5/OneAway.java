ad2df1ab4455363430849f1019b025eb505bc111
Tahir Ijaz <tahirijaz24@gmail.com>
2016-12-12 21:37:35 -0500

    starting

---
+++ chapter1/1.5/OneAway.java
@@ -0,0 +1,14 @@
+/*
+	[1.5]	Write a function to check if two strings are one edit away 
+			from each other - Insert, Remove, Replace character
+*/
+
+class OneAway {
+	private boolean isOneAway(Sting s1, String s2){
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
