eedb685fd5716deebb281b21818ad4483cd4cf67
tijazatrook <tahir.ijaz@rooksecurity.com>
2017-02-22 22:28:57 -0500

    its working, but not very optimized

--- ch1/1.8/ZeroMatrix.java
+++ ch1/1.8/ZeroMatrix.java
@@ -1,25 +1,43 @@
+/*
+	Problem 1.8
+	
+	If an element in a MxN matrix is 0, set that row and col 0
+	
+	Procedure: 	Using a MxN boolean array to save 0 locations
+				and using that to set rows, cols to zero
+			
+*/
+
 class ZeroMatrix {
 	private int[][] zeroMatrix(int[][] m){
 		int height = m.length;
 		int width = m[0].length;
+
+		boolean[][] is_zero = new boolean[height][width]; 
 		
 		for(int i=0; i<height; i++){
 			for(int j=0; j<width; j++){
-				if(m[i][j]==0){
-					for(int row=0; row < width; row++){
-						m[i][row] = 0;
-					}
-					for(int col=0; col < width; col++){
-						m[col][j] = 0;
-					}
-					i++;
-					j++;
-				}
-			}	
+				if (m[i][j] == 0)
+					is_zero[i][j] = true; 			
+			}
 		}
-		return m;	
+		
+		for(int i=0; i<height; i++){
+			for(int j=0; j<width; j++){
+				if((i > 0 && m[i-1][j]!=0) || (j > 0 && m[i][j-1]!=0)){ 
+					if (is_zero[i][j] == true){
+						for(int row=0; row<height; row++){
+							m[row][j] = 0;
+						}
+						for(int col=0; col<width; col++){
+							m[i][col] = 0;
+						}
+					}
+				}
+			}
+		}
+		return m;
 	}
-
 
 	private void printMatrix(int[][] matrix){
 		for(int i=0; i<matrix.length; i++){
@@ -31,7 +49,7 @@ class ZeroMatrix {
 	}
 	
 	public static void main(String[] args){
-		int[][] matrix = {{1, 2, 0}, {4, 1, 6}, {7, 8, 9}};
+		int[][] matrix = {{1, 2, 1, 3}, {4, 1, 6, 2}, {0, 8, 9, 4}};
 		ZeroMatrix r = new ZeroMatrix();
 		r.printMatrix(matrix);
 		System.out.println("----------------");
